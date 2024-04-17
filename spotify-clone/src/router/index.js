290145d374ac879e84ece7182b930f30906cd6d7
Jane Sardarian <sardarianjane@gmail.com>
2024-04-17 17:20:20 +0400

    Added the Library page and rendered songs

--- spotify-clone/src/router/index.js
+++ spotify-clone/src/router/index.js
@@ -1,19 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SearchView from '../views/SearchView.vue'
+import LibraryView from '../views/LibraryView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-
       component: HomeView,
     },
     {
       path: '/search',
-
       component: SearchView,
+    },
+    {
+      path: '/library',
+      component: LibraryView,
     },
   ],
 })
